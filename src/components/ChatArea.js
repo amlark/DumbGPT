5a21290bb13ee70e200cf4b2fb78f79860b3f635
amlark <79608631+amlark@users.noreply.github.com>
2023-03-15 06:57:04 +0100

    added key prop to parent element

--- src/components/ChatArea.js
+++ src/components/ChatArea.js
@@ -21,7 +21,10 @@ const ChatArea = ({ messages, onSendMessage }) => {
   };
 
   return (
-    <div className="bg-white rounded-lg shadow p-4 flex-grow ml-0 sm:ml-4 w-full sm:w-auto">
+    <div
+      key={messages.length}
+      className="bg-white rounded-lg shadow p-4 flex-grow ml-0 sm:ml-4 w-full sm:w-auto"
+    >
       <div className="overflow-y-auto h-72 sm:h-96 mb-4">
         {messages.map((message, index) => (
           <div
